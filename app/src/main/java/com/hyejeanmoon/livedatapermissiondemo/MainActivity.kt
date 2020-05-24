1a081da2032bc92f665a1173f5b5abaa48cbabfe
HyejeanMoon <wenhaichen1992@gmail.com>
2020-05-24 18:16:26 +0900

    update demo

--- app/src/main/java/com/hyejeanmoon/livedatapermissiondemo/MainActivity.kt
+++ app/src/main/java/com/hyejeanmoon/livedatapermissiondemo/MainActivity.kt
@@ -2,10 +2,11 @@ package com.hyejeanmoon.livedatapermissiondemo
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
+import com.hyejeanmoon.livedatapermission.EventObserver
 import com.hyejeanmoon.livedatapermission.LiveDataPermission
 import com.hyejeanmoon.livedatapermissiondemo.databinding.ActivityMainBinding
 
@@ -22,11 +23,34 @@ class MainActivity : AppCompatActivity() {
 
 
         binding.btPermission.setOnClickListener {
-            permission.init(this).permission(Manifest.permission.CAMERA).build()
+            permission.init(this)
+                .permission(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_CALENDAR
+                )
+                .requestShowExplanationMessage().build()
         }
 
-        permission.permissionResult.grantedPermissionList.observe(this, Observer {
-            Toast.makeText(this, it.peekContent().toString(), Toast.LENGTH_LONG).show()
+        permission.permissionResult.grantedPermissionList.observe(this, EventObserver {
+            Log.d("LiveDataPermission", "grantedPermissionList: ${it.size}")
+        })
+
+        permission.permissionResult.deniedPermissionList.observe(this, EventObserver {
+            Log.d("LiveDataPermission", "deniedPermissionList: ${it.size}")
+        })
+
+        permission.permissionResult.showExplanationMessageEvent.observe(this, EventObserver {
+            Log.d("LiveDataPermission", "showExplanationMessageEvent")
+        })
+
+        permission.permissionResult.allGranted.observe(this, EventObserver {
+            if (it) {
+                Toast.makeText(this, "AllGranted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "No AllGranted", Toast.LENGTH_LONG).show()
+            }
+
         })
     }
 
